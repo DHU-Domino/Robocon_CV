6a89b94a975d3d9e51399729f1a4bc16b3e22fb1
TTopoo <826955320@qq.com>
2021-04-27 17:52:11 +0800

    很烦

--- Robocon_CV/ImageConsProd.cpp
+++ Robocon_CV/ImageConsProd.cpp
@@ -117,46 +117,31 @@ void ImageConsProd::ImageConsumer(){
         }
         if (src.channels() != 3)
             continue;
-        //flip(src, src, 1);
-        //imshow_("src", src, isShow);
 
-        /*
-        //粉色
-        int iLowH1 = int((320 / 360.0) * 180), iHighH1 = int((360 / 360.0) * 180);
-        int iLowH2 = int((0 / 360.0) * 180), iHighH2 = int((3 / 360.0) * 180);
-        int iLowS = int(255 * 0.40), iHighS = int(255 * 0.70);
-        int iLowV = int(255 * 0.70), iHighV = int(255 * 1.00);
-        */
         //红色
-        int iLowH1 = int((354 / 360.0) * 180), iHighH1 = int((360 / 360.0) * 180);
-        int iLowH2 = int((0 / 360.0) * 180), iHighH2 = int((10 / 360.0) * 180);
+        //int iLowH1 = int((356 / 360.0) * 180), iHighH1 = int((360 / 360.0) * 180);
+        int iLowH2 = int((1 / 360.0) * 180), iHighH2 = int((10 / 360.0) * 180);
         int iLowS = int(255 * 0.55), iHighS = int(255 * 0.90);
         int iLowV = int(255 * 0.20), iHighV = int(255 * 1.00);
 
         Mat imgHSV1, imgHSV2, res1, res2, res3, res4, mask0, mask1, mask2, mask3;
         cvtColor(src, imgHSV1, COLOR_BGR2HSV);
         cvtColor(src, imgHSV2, COLOR_BGR2HSV);
-        inRange(imgHSV1, Scalar(iLowH1, iLowS, iLowV), Scalar(iHighH1, iHighS, iHighV), mask0);
+        //inRange(imgHSV1, Scalar(iLowH1, iLowS, iLowV), Scalar(iHighH1, iHighS, iHighV), mask0);
         inRange(imgHSV2, Scalar(iLowH2, iLowS, iLowV), Scalar(iHighH2, iHighS, iHighV), mask1);
 
-        add(mask0, mask1, mask1);
+        //add(mask0, mask1, mask1);
 
         bitwise_and(src, src, res1, mask1);
         imshow_("mask1", res1, isShow);
 
-        Mat element = getStructuringElement(MORPH_RECT, Size(5, 5));
-        Mat element1 = getStructuringElement(MORPH_RECT, Size(9, 9));
-        morphologyEx(mask1, mask2, MORPH_OPEN, element);//开操作
+        morphologyEx(mask1, mask2, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(15, 15)));//开操作
         bitwise_and(src, src, res2, mask2);
         imshow_("mask2", res2, isShow);
 
-        morphologyEx(mask2, mask3, MORPH_CLOSE, element1);//闭操作
+        morphologyEx(mask2, mask3, MORPH_CLOSE, getStructuringElement(MORPH_RECT, Size(200, 200)));
         bitwise_and(src, src, res3, mask2);
         imshow_("mask3", res3, isShow);
-
-
-        //-----------------------------------------------------------
-        
 
         vector<vector <Point> >contours;
         vector<Vec4i>hierarchy;
@@ -165,9 +150,9 @@ void ImageConsProd::ImageConsumer(){
         Point pt[512];//存储连通区域个数
         Moments moment;//矩
 
+        //读取每一个轮廓求面积，最大的部分截掉
         if (mInit != 2) {
-            for (int i = 0; i < contours.size(); i++) { //读取每一个轮廓求取重心
-
+            for (int i = 0; i < contours.size(); i++) {
                 if (mInit == 0) {
                     double t = contourArea(contours.at(i));
                     if (maxArea < t) {
@@ -179,17 +164,12 @@ void ImageConsProd::ImageConsumer(){
                     }
                 }
                 else if (mInit == 1) {
-                    //Point p = Point(maxCoor.x, maxCoor.y);//重心坐标
-                    //circle(src, p, 1, Scalar(255, 255, 255), 5, 8);//原图画出重心坐标
-                    //imshow_("haha", src, isShow);
-                    //waitKey(1);
-                    //cout << maxCoor << endl;
                     cv::Rect rect;
                     rect.width = VIDEO_WIDTH;
                     rect.height = maxCoor.y - 200;
                     rect.x = 0;
                     rect.y = 0;
-                    roiMask = mask3(rect);
+                    roiMask = mask2(rect);
                     //copyROI = mask3(rect);
                     imshow_("haha", roiMask, isShow);
                     mInit = 2;
@@ -197,7 +177,6 @@ void ImageConsProd::ImageConsumer(){
             }
             mInit = 1;
         }
-        
         contours.clear(); hierarchy.clear();
         //imshow_("roiMask", roiMask, isShow);
         /*
@@ -234,15 +213,58 @@ void ImageConsProd::ImageConsumer(){
         }*/
 
 
+        vector<RotatedRect> vrrect;
         findContours(roiMask, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);//查找轮廓
-        Point2d midBucket;
-        double minDx = 10000;
         for (int i = 0; i < contours.size(); i++) { //读取每一个轮廓求取重心
             Mat temp(contours.at(i));
-            moment = moments(temp, false);
-            if (contourArea(contours.at(i)) < 200) {
+            
+            double t = contourArea(contours.at(i));
+            if (t < 1000 || t >15000) {
                 continue;
             }
+            cout << t << " ";
+
+
+            RotatedRect _rect = minAreaRect(contours.at(i));
+            RotatedRect rect(_rect.center, _rect.size + Size2f(20, 10), _rect.angle);
+            
+            vrrect.push_back(rect);
+            //Rect perROI(rect);
+            Point2f p[4];
+            rect.points(p);
+            line(src, p[0], p[1], Scalar(0, 255, 0), 2);
+            line(src, p[1], p[2], Scalar(0, 255, 0), 2);
+            line(src, p[2], p[3], Scalar(0, 255, 0), 2);
+            line(src, p[3], p[0], Scalar(0, 255, 0), 2);
+            
+            imshow_("imgW", src, isShow);
+        }
+        cout << endl;
+        /*
+        while (vrrect.size() > 4) {//大临小，暴力匹配
+            int u = 0;
+            for (int i = 0; i < vrrect.size(); ++i) {
+                for (int j = i; j < vrrect.size(); ++j) {
+                    if (i == j) continue;
+                    double dis = pow((vrrect.at(i).center.x - vrrect.at(j).center.x), 2) + pow((vrrect.at(i).center.y - vrrect.at(j).center.y), 2);
+                    //cout << " i " << i << ",j" << j << ": "<< dis << " ";
+                    if (dis < 5000) {
+                        
+                        u = 1;
+                        break;
+                    }
+                }
+                if (u == 1)
+                    break;
+            }
+        }*/
+        
+        
+        imshow("out", src1);
+        ++csmIdx;
+        waitKey(1);
+        /*
+            moment = moments(temp, false);
             if (moment.m00 != 0) {//除数不能为0
                 pt[i].x = cvRound(moment.m10 / moment.m00);//计算重心横坐标
                 pt[i].y = cvRound(moment.m01 / moment.m00);//计算重心纵坐标
@@ -250,50 +272,8 @@ void ImageConsProd::ImageConsumer(){
                     minDx = (pt[i].x - VIDEO_WIDTH / 2.f);
                     midBucket = pt[i];
                 }
-
-                
-
-                RotatedRect rrrect = minAreaRect(contours.at(i));
-                RotatedRect rrect(rrrect.center, rrrect.size + Size2f(0, 10), rrrect.angle);
-                Point2f vertices0[4];
-                rrect.points(vertices0);
-                for (int i = 0; i < 4; i++)
-                    line(src, vertices0[i], vertices0[(i + 1) % 4], Scalar(0, 0, 255), 2);
-                
-                sort(vertices0, vertices0 + 4, [](Point2f& a, Point2f& b) { return a.x <= b.x; });
-                
-                //cout << vertices0[0] << vertices0[1] << vertices0[2] << vertices0[3] << endl;
-
-                Point2f tl(vertices0[0]), bl(vertices0[1]), tr(vertices0[2]), br(vertices0[3]);
-                
-                if (tl.y > bl.y) {
-                    swap(tl, bl);
-                }
-                if (tr.y > br.y) {
-                    swap(tr, br);
-                }
-                Point2f tmid(tl + (tr - tl) / 2.f), bmid(bl + (br - bl) / 2.f);
-
-                RotatedRect left(Point2f(tl.x , tl.y), Point2f(bl.x , bl.y), bmid);
-                RotatedRect right(Point2f(br.x , br.y), Point2f(tr.x , tr.y), tmid);
-                
-                Point2f vertices[4];
-                left.points(vertices);
-                for (int i = 0; i < 4; i++)
-                    line(src, vertices[i], vertices[(i + 1) % 4], Scalar(0, 255, 0), 2);
             }
-            Point p = Point(pt[i].x, pt[i].y);//重心坐标
-            circle(src, p, 1, Scalar(255, 255, 255), 5, 8);//原图画出重心坐标
-            imshow_("imgW", src, isShow);
-        }
-
-        //-----------------------------------------------------------
-
-        
-        imshow("out", src1);
-        ++csmIdx;
-        waitKey(1);
-
+        */
     }
 }
 
